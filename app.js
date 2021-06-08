411108ba8a7100384badf665ac4adb6bef67d404
nadirbashir <1.nadirbashir@gmail.com>
2021-06-08 23:11:32 +0500

    sample array object

--- app.js
+++ app.js
@@ -13,4 +13,30 @@ student.age = 16;
 
 // console.log(student);
 // console.log(student.email);
+// console.log("roll" in student); // check property name in object & return boolean value
 
+//object can be placed within an array
+
+var itemsArray = [
+    {
+        name:"juice",
+        price:50, 
+        quantity:3
+    }, 
+        {
+            name:"cookie",
+            price:30, 
+            quantity:9
+        },
+    {
+        name:"shirt",
+        price:880, 
+        quantity:1
+    },];
+
+// console.log(itemsArray[0].name);
+// console.log(itemsArray[0].price);
+// console.log(itemsArray[1].quantity);
+// console.log(itemsArray[1].price);
+// console.log(itemsArray[2].name);
+// console.log(itemsArray[2].quantity);
