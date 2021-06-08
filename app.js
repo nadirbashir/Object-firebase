2252db2858bc71f35ec83a1413ac862821dee8d0
nadirbashir <1.nadirbashir@gmail.com>
2021-06-08 22:47:27 +0500

    object initialize

---
+++ app.js
@@ -0,0 +1,16 @@
+//object is a type of var which has sets of values
+var student = {
+    lastName : "Ali",
+    grade : "12th",
+    age : 16,
+    email : "abc@gmail.com"
+};
+
+// var student = {};
+student.name = "Ahmed";
+student.roll = "567";
+student.age = 16;
+
+// console.log(student);
+// console.log(student.email);
+
